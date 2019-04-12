d898547f1ef65c08778787c82106f29dc444e3a1
mike <Mike DiDomenico>
2019-04-12 08:16:47 -0400

    laplacian edge test

--- project/testing/laplacian_edge_test.py
+++ project/testing/laplacian_edge_test.py
@@ -16,8 +16,16 @@ while(True):
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # edge detection
+    frame = cv2.Laplacian(frame, cv2.CV_64F)
+    frame = cv2.bitwise_not(frame)
+    # low pass filter
+    k = 1
+    kernel = np.ones((k,k),np.float32)/(k*k)
+    frame = cv2.filter2D(frame,-1,kernel)
+    # median filter
+    frame = cv2.medianBlur(np.float32(frame), 5)
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- project/testing/sobel_edge_test.py
+++ project/testing/sobel_edge_test.py
@@ -16,8 +16,11 @@ while(True):
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    sobelx = cv2.Sobel(frame,cv2.CV_64F,1,0,ksize=3)
+    sobely = cv2.Sobel(frame,cv2.CV_64F,0,1,ksize=3)
+    frame = np.power(np.power(sobelx,2)+np.power(sobely,2), 1/2)
+    
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
